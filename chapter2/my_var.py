0bbd121ea96689bf2de1b361c5509bea19f2426c
Lindokuhle <yendelindokuhle2@gmail.com>
2025-02-24 13:10:47 +0200

    Update my_var.py

--- chapter2/my_var.py
+++ chapter2/my_var.py
@@ -8,3 +8,5 @@ my_sentence = "cats are soo scary"
 print(my_sentence.title())
 
 ##title() is a string method that return the first letter of each word as an upper case
+
+print("Today is a brand new day. \nLet's enjoy today.\nRemain forever grateful")
