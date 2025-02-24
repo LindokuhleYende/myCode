ff1638cb7b2cafc515bebc331ca46a2f44855660
Lindokuhle <yendelindokuhle2@gmail.com>
2025-02-24 11:37:13 +0200

    Update my_var.py

--- chapter2/my_var.py
+++ chapter2/my_var.py
@@ -2,3 +2,9 @@ first_name = "Lindokuhle"
 last_name = "Yende"
 
 print("Hello "+first_name + " "+ last_name)
+
+my_sentence = "cats are soo scary"
+
+print(my_sentence.title())
+
+##title() is a string method that return the first letter of each word as an upper case
