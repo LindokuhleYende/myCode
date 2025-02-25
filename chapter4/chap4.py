91162c4ba64139572cb34f70dc265edab8f7db3f
Lindokuhle <yendelindokuhle2@gmail.com>
2025-02-25 13:10:47 +0200

    Working with for loops and lists

--- chapter4/chap4.py
+++ chapter4/chap4.py
@@ -2,3 +2,11 @@ fruits = ["Oranges", "Bananas", "Apples", "Strawberrys", "Grapes"]
 
 for x in fruits:
     print("Good day to eat some", x)
+
+
+pizzas = ["Pineapple", "Pepperoni", "cheese", "Beef", "Spicy chicken"]
+
+for pizza in pizzas:
+    print(f"I like {pizza} pizza.")
+
+print("I really like pizzas. \nPepperoni is my personal favourite!")
