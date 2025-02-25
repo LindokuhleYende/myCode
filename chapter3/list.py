6d5baa587aded0013f47f689796cb53fc66f716b
Lindokuhle <yendelindokuhle2@gmail.com>
2025-02-25 10:36:57 +0200

    Update list.py

--- chapter3/list.py
+++ chapter3/list.py
@@ -1,5 +1,5 @@
 my_class = ["Aidan", "Asanda", "Cadee", "Courtney", "Ethan", "Lesedi", "Lindo", "Marvelous", "Mieke", "Phomello","Sibu", "Ronny", "Tom", "Ulrich"]
-
+my_class.append("Everybody")
 for peer in my_class:
     print("Good Day", peer)
 
