402c4b6a43613b4673ff0ef96fce9903395f0c79
Lindokuhle <yendelindokuhle2@gmail.com>
2025-02-26 19:40:28 +0200

    Update chap7.py

--- chapter7/chap7.py
+++ chapter7/chap7.py
@@ -23,3 +23,9 @@ while prompt.lower() != "quit":
     toppings = input("Enter pizza toppings ")
     print(f"You will add {toppings} to your pizza")
     prompt = input("Enter quit if you want to stop adding pizza toppings ")
+
+# 7-5. Movie Tickets: A movie theater charges different ticket prices depending on
+# a person’s age. If a person is under the age of 3, the ticket is free; if they are
+# between 3 and 12, the ticket is $10; and if they are over age 12, the ticket is
+# $15. Write a loop in which you ask users their age, and then tell them the cost
+# of their movie ticket.
