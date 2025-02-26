58b851837a2c8d1a513f338960a5149bb6373851
Lindokuhle <yendelindokuhle2@gmail.com>
2025-02-26 14:10:34 +0200

    Created chap7.py

---
+++ chapter7/chap7.py
@@ -0,0 +1,9 @@
+car = input("What type of car do you want to rent? ")
+print(f"Let me see if I can find you a {car}.")
+
+resturant = int(input("How many guests are in your dinner group: "))
+
+if resturant>8:
+    print("Please wait for a table!")
+else:
+    print("Your table is ready.. ")
