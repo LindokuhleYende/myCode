2f02b5a40ca2ed32e32d49faaae3b1020b8f6bd2
Lindokuhle <yendelindokuhle2@gmail.com>
2025-02-26 14:24:55 +0200

    Update chap7.py

--- chapter7/chap7.py
+++ chapter7/chap7.py
@@ -11,6 +11,6 @@ else:
 num = int(input("Please enter any number: "))
 
 if num%10 == 0:
-    print(f"The number {num} is a multIiple of 10")
+    print(f"The number {num} is a multiple of 10")
 else:
     print(f"The number {num} is not a multiple of 10")
