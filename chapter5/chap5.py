3f833220288e3552d273f5ecccf62c7d5d032546
Lindokuhle <yendelindokuhle2@gmail.com>
2025-02-26 10:01:52 +0200

    Created chap5.py

---
+++ chapter5/chap5.py
@@ -0,0 +1,14 @@
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+age = 54
+
+if age>60:
+    print("You are old")
+elif age<60 and age>40:
+    print("Enjoy mid life")
+else:
+    print("You have soo much to live for!")
