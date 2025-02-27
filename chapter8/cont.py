a30320d15c6aec4e27d1136bd164affd20045d91
Lindokuhle <yendelindokuhle2@gmail.com>
2025-02-27 20:46:16 +0200

    Created cont.py

---
+++ chapter8/cont.py
@@ -0,0 +1,12 @@
+# 8-12. Sandwiches: Write a function that accepts a list of items a person wants
+# on a sandwich. The function should have one parameter that collects as many
+# items as the function call provides, and it should print a summary of the sandwich
+# that’s being ordered. Call the function three times, using a different number
+# of arguments each time.
+sandwich_list = []
+def sandwiches(*sandwich):
+    sandwich_list.append(sandwich)
+    print(f"This is what I want in my sandwich: {sandwich_list}")
+sandwiches("tomato", "cheese")
+sandwiches("bacon", "sauce", "chips")
+sandwiches("cheese", "polony", "egg", "russian")
