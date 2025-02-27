68e09aca54ab5d435c5b0bff4d176abbd52413a0
Lindokuhle <yendelindokuhle2@gmail.com>
2025-02-27 09:55:03 +0200

    Update func.py

---
+++ chapter8/func.py
@@ -0,0 +1,11 @@
+def display_message():
+    print("We are learning about functions in chapter8")
+display_message()
+
+def favorite_book(title):
+    print("The name of my favorite books is", title)
+favorite_book("It ends with us")
+
+def describe_city(city, country):
+    print(f"{city} is in {country}")
+describe_city("Cpt", "RSA")
