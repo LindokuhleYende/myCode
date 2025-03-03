e2422696a7b00286f62bc4fa6271790c578ae9a9
Lindokuhle <yendelindokuhle2@gmail.com>
2025-03-03 13:13:34 +0200

    Update file_reader.py

--- chapter10/file_reader.py
+++ chapter10/file_reader.py
@@ -4,7 +4,7 @@ from pathlib import Path
 path = Path('lindokuhle.txt')
 contents = path.read_text()
 
-# lines = contents.splitlines()
-# for line in lines:
-    # print(line)
+lines = contents.splitlines()
+for line in lines:
+    print(line)
 print(contents)
