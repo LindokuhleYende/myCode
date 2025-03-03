486cb9f3c0b1134f2b3643e75b00dea97ab7b20d
Lindokuhle <yendelindokuhle2@gmail.com>
2025-03-03 09:53:29 +0200

    Upated module resturant

--- chapter9/res.py
+++ chapter9/res.py
@@ -7,14 +7,14 @@ class Resturant:
     def open_restaurant(self):
         print(f"{self.res_name} is open for business from 9am")
 
-eat = Resturant("Pizza hut", "italian")
-eat.describe_restaurant()
-eat.open_restaurant()
-
-dine = Resturant("Sushi world", "Japanese")
-dine.describe_restaurant()
-dine.open_restaurant()
-
-lunch = Resturant("Taco town", "Mexican")
-lunch.describe_restaurant()
-lunch.open_restaurant()
+# eat = Resturant("Pizza hut", "italian")
+# eat.describe_restaurant()
+# eat.open_restaurant()
+# 
+# dine = Resturant("Sushi world", "Japanese")
+# dine.describe_restaurant()
+# dine.open_restaurant()
+# 
+# lunch = Resturant("Taco town", "Mexican")
+# lunch.describe_restaurant()
+# lunch.open_restaurant()
--- chapter9/try1.py
+++ chapter9/try1.py
@@ -1,1 +1,5 @@
 from res import Resturant as re
+
+eat = re("Pizza hut", "italian")
+eat.describe_restaurant()
+eat.open_restaurant()
