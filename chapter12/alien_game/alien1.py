d7d341aa3ba4ee95761b72e31f8b426092621c95
Lindokuhle <yendelindokuhle2@gmail.com>
2025-03-04 14:42:50 +0200

    Update alien1.py

--- chapter12/alien_game/alien1.py
+++ chapter12/alien_game/alien1.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from settings import Settings
 class AlienInvasion:
    # """Overall class to manage game assets and behavior."""
    def __init__(self):
@@ -10,17 +11,20 @@ class AlienInvasion:
       self.clock = pygame.time.Clock()
       # Set the background color.
       self.bg_color = (230, 230, 230)
+      self.settings = Settings()
+      self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
    def run_game(self):
      """Start the main loop for the game."""
      while True:
         # Watch for keyboard and mouse events.
         for event in pygame.event.get():
+           self.screen.fill(self.settings.bg_color)
            if event.type == pygame.QUIT:
               sys.exit()
               # Make the most recently drawn screen visible.
               pygame.display.flip()
               self.clock.tick(60)
-        self.screen.fill(self.bg_color)
+      #   self.screen.fill(self.bg_color)
 if __name__ == '__main__':
    # Make a game instance, and run the game.
    ai = AlienInvasion()
