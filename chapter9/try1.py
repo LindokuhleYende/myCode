4c206c5afdea1a4ad1bba7a9fe53cd1ffec70808
Lindokuhle <yendelindokuhle2@gmail.com>
2025-03-04 22:47:20 +0200

    Updated the resturant class

--- chapter9/try1.py
+++ chapter9/try1.py
@@ -3,3 +3,7 @@ from res import Resturant as re
 eat = re("Pizza hut", "italian")
 eat.describe_restaurant()
 eat.open_restaurant()
+
+dine = re("Sushi world", "Japanese")
+dine.describe_restaurant()
+dine.open_restaurant()
