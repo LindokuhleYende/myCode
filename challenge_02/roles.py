58afc198c266adb337b188698996c39db838b8e1
Lindokuhle <yendelindokuhle2@gmail.com>
2025-03-04 10:03:21 +0200

    Challenge 2

---
+++ challenge_02/roles.py
@@ -0,0 +1,8 @@
+class Role:
+
+    def __init__(self, position_name):
+        self.position_name = position_name
+
+janitor = Role("Janitor")
+programmer = Role("Programmer")
+secretory = Role("Secretory")
---
+++ challenge_02/sign_up.py
@@ -0,0 +1,26 @@
+from roles import janitor, programmer, secretory
+
+class SignUp:
+
+    def __init__(self):
+        self.first_name = input("What is your first name: ")
+        self.second_name = input("What is your last name: ")
+        self.email = input("What is your email: ")
+        
+        roles = {
+            "Janitor": janitor,
+            "Programmer": programmer,
+            "Secretory": secretory
+        }
+
+        print("\nAvailable Positions:")
+        for role_name in roles:
+            print(f"- {role_name}")
+
+        while True:
+            chosen_role = input("Select your position: ").strip().title()
+            if chosen_role in roles:
+                self.position = roles[chosen_role]
+                break
+            else:
+                print("Invalid choice. Please select a valid position.")
