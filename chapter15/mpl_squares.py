8094d11407939d9cfcf94ed0afe9a8ceb0b3e701
Lindokuhle <yendelindokuhle2@gmail.com>
2025-03-05 17:22:25 +0200

    Update mpl_squares.py

--- chapter15/mpl_squares.py
+++ chapter15/mpl_squares.py
@@ -1,11 +1,18 @@
 import matplotlib.pyplot as plt
 
 square = [1, 4, 9, 16, 25]
+# input_values = [1, 2, 3, 4, 5]
 fig, ax = plt.subplots()
+# 
+# plt.style.use('seaborn-v0_8')
+# ax.plot(input_values, square, linewidth=3)
 ax.plot(square)
 plt.show()
 
 # Set chart title and label axes.
-ax.set_title("Square Numbers", fontsize=24)
-ax.set_xlabel("Value", fontsize=14)
-ax.set_ylabel("Square of Value", fontsize=14)
+plt.title("Square Numbers")
+plt.xlabel("Value")
+plt.ylabel("Square of Value")
+
+# Set size of tick labels.
+# ax.tick_params(labelsize=14)
