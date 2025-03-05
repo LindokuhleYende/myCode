ce9d35371c0838f294900893ccd1082460c16636
Lindokuhle <yendelindokuhle2@gmail.com>
2025-03-05 13:21:54 +0200

    Update ship.py

--- chapter13/ship.py
+++ chapter13/ship.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 class Ship:
     """A class to manage the ship."""
 
@@ -13,27 +14,24 @@ class Ship:
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 
-        # Resize the ship image to the specified dimensions
-        self.image = pygame.transform.scale(self.image, (self.settings.ship_width, self.settings.ship_height))
-        self.rect = self.image.get_rect()
-
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
-        # Store a decimal value for the ship's horizontal position.
+        # Store a float for the ship's exact horizontal position.
         self.x = float(self.rect.x)
 
-        # Movement flags
+        # Movement flags; start with a ship that's not moving.
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
-        """Update the ship's position based on the movement flags."""
+        """Update the ship's position based on movement flags."""
+        # Update the ship's x value, not the rect.
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
-
+            
         # Update rect object from self.x.
         self.rect.x = self.x
 
