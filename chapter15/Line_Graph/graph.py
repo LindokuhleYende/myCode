78b11d0cd10f0bcd9034e3d267e6dcfe444b13b4
Lindokuhle <yendelindokuhle2@gmail.com>
2025-03-08 19:48:05 +0200

    Create graph.py

---
+++ chapter15/Line_Graph/graph.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+x_values = range(-220, 300)
+y_values = [x**3 for x in x_values]
+fig, ax = plt.subplots()
+ax.plot(x_values, y_values, color="yellow")
+ax.set_title("Cubic Function")
+ax.set_xlabel("X-Values")
+ax.set_ylabel("Y-Values")
+ax.set_facecolor("blue")
+plt.grid()
+plt.show()
