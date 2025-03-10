3ee10e681b56e02f081ffb4227fdfdb4c2e17cfc
Lindokuhle <yendelindokuhle2@gmail.com>
2025-03-10 11:21:50 +0200

    Update graph3.py

--- chapter15/Line_Graph/graph3.py
+++ chapter15/Line_Graph/graph3.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 # javascript = np.array([2, 6, 5, 9, 8])
 # html = np.array([1, 10, 8 ])
 labels = ["Python", "Javascript", "Html", "Css"]
-sizes = [50, 25,0, 25]
+sizes = [40,20,20,20]
 plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=140)
 plt.title("Favourite Languages")
 plt.show()
