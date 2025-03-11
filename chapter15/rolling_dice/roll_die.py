f38dc2945a5c27ad91692409fa9873e517b39606
Lindokuhle <yendelindokuhle2@gmail.com>
2025-03-11 09:35:59 +0200

    Update roll_die.py

--- chapter15/rolling_dice/roll_die.py
+++ chapter15/rolling_dice/roll_die.py
@@ -1,2 +1,9 @@
-import pandas as pd
-print(pd.__version__)
+from random import randint
+
+class Die:
+    def __init__(self, num_sides=6):
+        self.num_sides = num_sides
+    def roll(self):
+        #return a random integer between 1 and 6(by default)
+        #num_sides = 6 unless user changes it by inputing argument
+        return randint(1, self.num_sides)
