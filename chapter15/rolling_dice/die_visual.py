d0a91a89774a7562b64dc703ee3c5bd5eb858049
Lindokuhle <yendelindokuhle2@gmail.com>
2025-03-11 09:47:07 +0200

    Update die_visual.py

--- chapter15/rolling_dice/die_visual.py
+++ chapter15/rolling_dice/die_visual.py
@@ -3,5 +3,14 @@ from roll_die import Die
 #By default the num_sides =6 
 die = Die()
 
-results =[die.roll() for roll_num in range(10)]
-print(results)
+results =[die.roll() for roll_num in range(100)]
+#print(results)
+
+#Analysing the results
+frequencies = []
+poss_values = range(1, die.num_sides+1)
+
+for value in poss_values:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+#print(frequencies)
