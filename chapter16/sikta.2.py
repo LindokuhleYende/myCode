f848d522c2471cc9447957cb6f2ce84fe3245977
Lindokuhle <yendelindokuhle2@gmail.com>
2025-03-12 14:01:10 +0200

    Update sikta.2.py

--- chapter16/sikta.2.py
+++ chapter16/sikta.2.py
@@ -4,3 +4,7 @@ import matplotlib.pyplot as plt
 filepath = r'C:\Users\Lindokuhle Yende\OneDrive\Desktop\CodeCollege\python\myCode\chapter16\weather_data\sitka_weather_2021_full.csv'
 df = pd.read_csv(filepath)
 print(df.head())
+
+df["DATE"] = pd.to_datetime(df['DATE'])
+plt.plot(df['DATE'], df["PRCP"])
+plt.show()
