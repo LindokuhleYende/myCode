5ae3975d2f5923f56dfce2f295e54a2c0b6eba21
Lindokuhle <yendelindokuhle2@gmail.com>
2025-03-17 13:22:07 +0200

    Update views.py

--- chapter18/learning_log/learning_logs/views.py
+++ chapter18/learning_log/learning_logs/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Topic, Entry
 from .forms import TopicForm,EntryForm
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 # Create your views here.
 def index(request):
