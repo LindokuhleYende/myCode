47c329fd6860980ddd917b06e93f54b7ff679b59
Lindokuhle <yendelindokuhle2@gmail.com>
2025-03-17 13:26:18 +0200

    Update views.py

--- chapter18/learning_log/learning_logs/views.py
+++ chapter18/learning_log/learning_logs/views.py
@@ -37,7 +37,9 @@ def new_topic(request):
        # POST data submitted; process data.
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
     # Display a blank or invalid form.
     context = {'form': form}
@@ -68,6 +70,8 @@ def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
     if request.method != 'POST':
         # Initial request; pre-fill form with the current entry.
         form = EntryForm(instance=entry)
